72c09c93edcfe66ea6fde61d054e6e3f3e574315
Nickey <nickey@Nickeys-MacBook-Pro.local>
2025-12-30 20:59:29 +0630

    config

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://barcode-front-vercel.vercel.app/',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
